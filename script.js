e9c454759bb8c441456f925c39521a182445f8e2
TMYTiMidlY <93774927+TMYTiMidlY@users.noreply.github.com>
2023-12-30 19:57:50 +0800

    添加声明和完善搜索列表

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const params = new URLSearchParams(window.location.search);
 const searchQuery = params.get('q');
 const courseID = params.get('id');
-const code = params.get('code');
+const resultsEl = document.getElementById('courseList');
 const help = document.getElementById('help');
 
 if (searchQuery) {
@@ -13,65 +13,58 @@ if (searchQuery) {
             let searchResults = data.data.items.filter(course => course.name.includes(searchQuery) || course.code.includes(searchQuery));
             console.log(searchResults)
 
-            var ul = document.getElementById('courseList');
 
-            searchResults.forEach((course) => {
-                var li = document.createElement('li');
+            searchResults.forEach(course => {
+                var card = document.createElement('div');
+                card.classList.add('card');
+                card.style.border = '1px solid #ccc';
+                var nameEl = document.createElement('h3');
+                nameEl.textContent = course.name;
+                card.className = 'card';
+                var codeLink = document.createElement('a');
+                codeLink.href = `${window.location.origin}/TMYTiMidlY.github.io/?id=${course.id}`;
+                codeLink.textContent = course.code;
 
-                var a = document.createElement('a');
-                a.textContent = course.name;
-                a.href = `${window.location.origin}/TMYTiMidlY.github.io/?id=${course.id}`;;
+                card.appendChild(nameEl);
+                card.appendChild(codeLink);
 
-                li.appendChild(a)
-                ul.appendChild(li);
+                resultsEl.appendChild(card);
             });
         });
 
 } else if (courseID) {
     help.style.display = 'block'
+
+    var loading = document.createElement('h3');
+    loading.textContent = '加载中...';
+    resultsEl.appendChild(loading);
+
     fetch(`https://v.ustc.edu.cn/api/v1/course/${courseID}/schedules`)
         .then(response => response.json())
         .then(data => {
-            var ul = document.getElementById('courseList');
+            resultsEl.removeChild(loading)
 
-            data.data.forEach((schedule) => {
-                var li = document.createElement('li');
+            console.log(data)
+            data.data.forEach(schedule => {
+                var card = document.createElement('div');
+                card.classList.add('card');
+                card.style.border = '1px solid #ccc';
+                var nameEl = document.createElement('h3');
 
-                var a = document.createElement('a');
-                a.textContent = schedule.start_time;
-                a.href = `https://v.ustc.edu.cn/api/v1/captures/${schedule.capture_code}/capture-iframe-url?language=zh-CN&is_lesson_resource=false`
-                a.target = "_blank";
+                var start_date = new Date(schedule.start_time);
+                var end_date = new Date(schedule.end_time);
+                nameEl.textContent = `${start_date.toLocaleDateString()} ${start_date.toLocaleTimeString()} - ${end_date.toLocaleTimeString()} `;
 
-                // a.addEventListener('click', function (e) {
-                //     e.preventDefault();
-                //     fetch(`https://v.ustc.edu.cn/api/v1/captures/${schedule.capture_code}/capture-iframe-url?language=zh-CN&is_lesson_resource=false`)
-                //         .then(response => response.json())
-                //         .then(data => {
-                //             if (data.error) {
-                //                 alert(data.error.message)
-                //             }
-                //         })
-                // });
+                card.className = 'card';
+                var codeLink = document.createElement('a');
+                codeLink.href = `https://v.ustc.edu.cn/api/v1/captures/${schedule.capture_code}/capture-iframe-url?language=zh-CN&is_lesson_resource=false`;
+                codeLink.target = "_blank";
+                codeLink.textContent = `week ${schedule.week}, ${schedule.status}`;
 
-                li.appendChild(a)
-                ul.appendChild(li);
+                card.appendChild(nameEl);
+                card.appendChild(codeLink);
+
+                resultsEl.appendChild(card);
             });
         });
-} else if (code) {
-    console.log(1)
-    async function loadData() {
-        let response = await fetch('./cources.json');
-        let data = await response.json();
-        console.log(data)
-        let results = {
-            "data": data.data.items.filter(course => course.code.includes(code))[0], "error": {
-                "code": 0,
-                "details": {},
-                "message": "",
-                "status": "OK"
-            }
-        };
-        document.body.innerText = JSON.stringify(results, null, 2);
-    }
-    window.onload = loadData;
 }
